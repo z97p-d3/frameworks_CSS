813ee058bd69de99db69c20bd1fe518163343c19
Dario Pilacuan <dario.pilacuan@epn.edu.ec>
2018-05-12 22:24:15 -0500

    2.0
    versiones completa de bootstrap fundation materialize

---
+++ Blog_de_turismo_materialize/ccs-materialize/js/estilo.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+      
